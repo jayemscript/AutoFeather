d05a6a1f1ad3836e1ec3fc35a205137dd1a93e3a
jayemscript <johnmarkpulmano052802@gmail.com>
2026-02-12 15:47:33 +0800

    aa

--- client/src/components/navigation/private_navs/tab-side-bar.tsx
+++ client/src/components/navigation/private_navs/tab-side-bar.tsx
@@ -164,7 +164,7 @@ export default function TabSideBar({ isOpen, onClose }: TabSideBarProps) {
             className="flex flex-col w-64 h-full p-5 space-y-6 shadow-lg bg-primary dark:bg-background text-accent dark:text-white relative"
           >
             <div className="flex items-center space-x-2 mb-6">
-              <img src="/images/logo.webp" alt="Logo" className="h-8 w-8" />
+              <img src="/images/logo.jpg" alt="Logo" className="h-8 w-8" />
               {isOpen && (
                 <span className="font-bold text-[0.7em]">AutoFeather</span>
               )}
